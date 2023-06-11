efc29eb1e0ac6d1e7d8670cf319310c1a7111ad3
InnovativeSharks <manmarius199@gmail.com>
2023-06-11 09:58:46 +0300

    commit examen 1

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. Exam only coding for
-          this
+          sadsadasda this
         </p>
         <a
           className="App-link"
